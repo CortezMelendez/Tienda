204c5c9eba8d7b331757e143f6ac6b848582fe7d
CortezMelendez <20253rd043@utez.edu.mx>
2026-05-13 11:54:57 -0600

    Agregacion del scanner

--- src/Main.java
+++ src/Main.java
@@ -12,17 +12,17 @@ public static void main(String[] args) {
 
     producto.mostrarInfo();
     producto.calcularValorInventario();
-    producto.aplicarDescuento();
+    producto.aplicarDescuento(descuento);
     producto.hayDisponibilidad();
     System.out.println("===========================================");
     producto1.mostrarInfo();
     producto1.calcularValorInventario();
-    producto1.aplicarDescuento();
+    producto1.aplicarDescuento(descuento);
     producto1.hayDisponibilidad();
     System.out.println("===========================================");
     producto2.mostrarInfo();
     producto2.calcularValorInventario();
-    producto2.aplicarDescuento();
+    producto2.aplicarDescuento(descuento);
     producto2.hayDisponibilidad();
 }
 }
